e589d2cc1308ae950f1fbb8f48d0ccf94142cb0b
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-07-07 13:01:48 +0530

    Update Insights.jsx

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -17,7 +17,7 @@ const Insights = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
       <TypingText title="| Insight" textStyles="text-center" />
-      <TitleText title="Insight about Metaverse" textStyles="text-center" />
+      <TitleText title="Insight about Vegamedia" textStyles="text-center" />
       <div className="mt-[50px] flex flex-col gap-[30px]">
         {insights.map((insight, i) => (
           <InsightCard key={`insight-${i}`} {...insight} index={i + 1} />
