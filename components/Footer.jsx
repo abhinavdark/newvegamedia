95c056b73bf6d6af2117856804252dd32d2022eb
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-07-07 12:28:00 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -18,17 +18,22 @@ const Footer = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-           Enter your digital marketing journey 
+          Enter your  digital  marketing joutney
         </h4>
 
         <button
           type="button"
           className="flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px]"
         >
-        
+          <img
+            src="/headset.svg"
+            alt="Headset"
+            className="w-[24px] h-[24px] object-contain"
+          />
           <span className="font-normal text-[16px] text-white text-uppercase">
-            Enter your digital marketing journey 
+            Let's Talk
           </span>
+           <a href="https://calendly.com/vegamediain/30min target">
         </button>
       </div>
 
@@ -36,11 +41,11 @@ const Footer = () => (
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">Metaversus</h4>
+          <h4 className="font-extrabold text-[24px] text-white">Vegamedia</h4>
           <p className="font-normal text-[14px] text-white opacity-50">
             Copyright &copy; {new Date().getFullYear()} -{" "}
             {new Date().getFullYear() + 1}{" "}
-            <span className="font-extrabold">Metaversus</span>. All Rights
+            <span className="font-extrabold">vegamedia</span>. All Rights
             Reserved.
           </p>
 
