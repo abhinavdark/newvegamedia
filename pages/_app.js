81fcd263db2897eeec6b086cdeb53f834a369516
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-07-07 13:40:18 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,6 @@ import Head from "next/head";
 
 import "../styles/globals.css";
 
-import "./sendinblue";
-
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
@@ -69,7 +67,37 @@ const MyApp = ({ Component, pageProps }) => (
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </Head>
-    <Component {...pageProps} />
+    <body data-spy="scroll" data-target="#navbarSupportedContent" data-offset="90">
+      <script type="text/javascript">
+        (function() {
+          window.sib = {
+            equeue: [],
+            client_key: "c0cr519o5kneubqda7gjp3l7"
+          };
+          /* OPTIONAL: email for identify request*/
+          // window.sib.email_id = 'example@domain.com';
+          window.sendinblue = {};
+          for (var j = ['track', 'identify', 'trackLink', 'page'], i = 0; i < j.length; i++) {
+            (function(k) {
+              window.sendinblue[k] = function() {
+                var arg = Array.prototype.slice.call(arguments);
+                (window.sib[k] || function() {
+                  var t = {};
+                  t[k] = arg;
+                  window.sib.equeue.push(t);
+                })(arg[0], arg[1], arg[2]);
+              };
+            })(j[i]);
+          }
+          var n = document.createElement("script"),
+            i = document.getElementsByTagName("script")[0];
+          n.type = "text/javascript", n.id = "sendinblue-js",
+          n.async = !0, n.src = "https://sibautomation.com/sa.js?key=" + window.sib.client_key,
+          i.parentNode.insertBefore(n, i), window.sendinblue.page();
+        })();
+      </script>
+      <Component {...pageProps} />
+    </body>
   </>
 );
 
