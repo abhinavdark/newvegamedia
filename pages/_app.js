9f931dcec07779a2819c737c99d1d811603b02dc
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-07-07 13:54:21 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -66,14 +66,7 @@ const MyApp = ({ Component, pageProps }) => (
       <link rel="icon" type="image/x-icon" href="./assets/images/favicon/favicon.ico" />
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-72PC6FML97"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-72PC6FML97');
-</script>
     </Head>
     <Component {...pageProps} />
   </>
