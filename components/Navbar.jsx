043b384362251c6e47cc92bf8d31b7c29cc9939d
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-07-07 12:36:32 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,11 +13,25 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+     
+        className="w-[24px] h-[24px] object-contain"
+      />
       <h2 className="font-extrabold text-[24px] loading-[30px] text-white">
         VEGAMEDIA
       </h2>
-     
+      <a
+        href="https://github.com/sanidhyy/metaversus"
+        target="_blank"
+        rel="noreferrer noopener"
+        title="Source Code"
+      >
+        <img
+          
+          className="w-[24px] h-[24px] object-contain"
+        />
+      </a>
     </div>
   </motion.nav>
 );
