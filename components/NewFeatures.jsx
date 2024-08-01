c0ff9c740872a924cfe1309d371fad4108f794fe
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-08-01 23:05:49 +0530

    Update NewFeatures.jsx

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,5 +1,3 @@
-import styles from "../styles";
-
 const NewFeatures = ({ title, subtitle }) => (
   <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
     <h1 className="mt-[26px] font-bold text-[24px] leading-[30px] text-white">
