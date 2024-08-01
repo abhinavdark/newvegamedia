a64dee428771dbb68cebfa403ba1a11cce588486
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-08-01 23:05:05 +0530

    Update NewFeatures.jsx

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -2,7 +2,6 @@ import styles from "../styles";
 
 const NewFeatures = ({ title, subtitle }) => (
   <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
-    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`} />
     <h1 className="mt-[26px] font-bold text-[24px] leading-[30px] text-white">
       {title}
     </h1>
