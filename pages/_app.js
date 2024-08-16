cccd618640e18a8772102fde2a477a694dfbb868
Abhinav <63947769+abhinavdark@users.noreply.github.com>
2024-08-16 20:47:46 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import "../styles/globals.css";
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
-      <title>VegaMedia | Digital marketing</title>
+      <title>VegaMedia | Digital marketing.</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
       <meta charSet="UTF-8" />
